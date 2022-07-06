0147b5b3ded65c1e7467e9e823439cb9cb981d93
Danila Kalay <ChechikLud@gmail.com>
2022-07-06 14:11:58 +0300

    Post commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.name = "Иван";
+        post.surname = "Иванов";
+        post.patronymic = "Васильевич";
+        post.passport = "5555№555555";
+        post.phone = "+7 (999)-888-77-66";
+        post.subscription = true;
+
+        post.birthday.day = 15;
+        post.birthday.month = 12;
+        post.birthday.year = 1977;
+
+
     }
 }
